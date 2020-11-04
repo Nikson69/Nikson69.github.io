82ffea330360e89c39002350acb54a59e73dd95b
Nikita Nazarov <nnazarov@gmcs.ru>
2020-11-04 22:33:21 +0300

    add img update packege

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './images/Nikson69Logo.png';
+import './index.css';
  
 const title = 'Привет, скоро тут что-нибудь должно быть (:';
  
 ReactDOM.render(
-  <div>{title}</div>,
+  <div className="temp" >
+    <img class="loader-img" src={logo}/>
+    {title}
+    </div>,
   document.getElementById('app')
 );
