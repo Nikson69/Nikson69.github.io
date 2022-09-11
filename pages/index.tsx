efd9f4b6022ee9a6af20e481bad85b56068bf213
Nikson69 <Nikitos71ru@gmail.com>
2022-09-11 23:33:25 +0300

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,26 @@ import Image from 'next/image'
 import profilePic from '../public/images/Nikson69Logo.png'
 import bg from '../public/images/background.jpg'
 import style from '../public/styles/index.module.scss'
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }) {
-  return <HomePage />
+export default function MyApp() {
+
+  return <React.Fragment>
+    <Head>
+      <title>Nikson69</title>
+      <link href="/favicon.ico" rel="icon" type="image/x-icon"/>
+    </Head>
+    <HomePage />
+  </React.Fragment>
 }
 
 export function HomePage() {
   return (
-    <div className={style.temp} style={{ backgroundImage:`url(${bg.src})`}} >
+    <div className={style.temp} style={{ backgroundImage: `url(${bg.src})` }} >
       <div className={style.image}>
-        <Image 
+        <Image
           layout='responsive'
-          src={profilePic} 
+          src={profilePic}
           alt="Picture of the author"
         />
       </div>
